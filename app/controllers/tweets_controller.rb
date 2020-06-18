f838ea39946147c596b1cfdb11c8f1dec5853451
goodriceball2100 <63901267+goodriceball2100@users.noreply.github.com>
2020-06-18 16:53:31 +0900

    setting:edit_destroy_show

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,5 @@
 class TweetsController < ApplicationController
-
+  before_action :set_tweet, only: [:edit, :show]
   def index
     @tweets = Tweet.all
   end
@@ -12,8 +12,28 @@ class TweetsController < ApplicationController
     Tweet.create(tweet_params)
   end
 
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+  end
+
+  def edit
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+  end
+
+  def show
+  end
+
   private
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id]) 
   end
 end
